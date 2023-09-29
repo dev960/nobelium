39c26392a3e4d23bcf0aa9acbdfc25e802ef4d8a
dev960 <67815161+dev960@users.noreply.github.com>
2023-09-29 19:46:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '272c76d1-5b7a-4614-9596-0c248f0159a6', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
